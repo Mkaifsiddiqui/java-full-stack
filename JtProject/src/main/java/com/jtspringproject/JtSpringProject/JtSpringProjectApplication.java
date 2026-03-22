8c9d6ef79c71ac01da9a7d78d257fa93a76b7cce
Mkaifsiddiqui <98751793+Mkaifsiddiqui@users.noreply.github.com>
2026-03-22 20:27:01 +0530

    Fix formatting in JtSpringProjectApplication.java

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
@@ -3,7 +3,7 @@ package com.jtspringproject.JtSpringProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class JtSpringProjectApplication {
 
 	public static void main(String[] args) {
